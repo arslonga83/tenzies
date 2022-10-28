e17a739e1a873c73910d01c89c0b6a3ba67d39ca
arslonga83 <jlwood83@gmail.com>
2022-10-27 22:46:09 -0700

    build placeholder dice and random number function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 
 import './App.css'
+import Die from './Components/Die'
 
 function App() {
 
+  function allNewDice() {
+    const dice = [];
+    for (let i = 0; i < 10; i++) {
+      dice.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return dice;
+  }
+  
+  console.log(allNewDice());
+
   return (
-    <main className="main">
-      
+    <main>
+      <div className='container'>
+        <Die value={6} />
+        <Die value={3} />
+        <Die value={2} />
+        <Die value={1} />
+        <Die value={5} />
+        <Die value={3} />
+        <Die value={1} />
+        <Die value={4} />
+        <Die value={6} />
+        <Die value={4} />
+      </div>
     </main>
   )
 }
