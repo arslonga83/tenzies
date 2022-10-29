f47bbd0504d907e4d7d65eb8cce95d32def57e99
arslonga83 <jlwood83@gmail.com>
2022-10-28 23:40:13 -0700

    add instructions and style

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,10 @@ function App() {
   
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='container'>
-      {diceElements}
+        {diceElements}
       </div>
       <button onClick={rollDice}>Roll</button>
     </main>
